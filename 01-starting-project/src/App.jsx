088b70885b342c16096fd85badf124870941f8f1
pcbimon <patipat.che@mahidol.edu>
2023-12-11 14:59:21 +0700

    add onclick

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,6 +3,9 @@ import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 function App() {
+  function handleSelect() {
+    console.log('Hello World - selected')
+  }
   return (
     <div>
       <Header />
@@ -32,12 +35,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
+        Dynamic Content
       </main>
     </div>
   );
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,10 @@
-export default function TabButton({children}) {
+export default function TabButton({children,onSelect}) {
+    function handleClick() {
+        console.log('clicked')
+    }
     return (
         <li>
-            <button>{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     )
 }
