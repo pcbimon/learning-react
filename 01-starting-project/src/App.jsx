a5b98ba6f07432fe04f57997955ca26a5d0e3b45
pcbimon <patipat.che@mahidol.edu>
2023-12-11 21:18:53 +0700

    useState to update some data in component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,9 +2,13 @@ import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
 function App() {
-  function handleSelect() {
-    console.log('Hello World - selected')
+  //state
+  const [ selectedTopic,setSekectedTopic ] = useState('Please click a button');
+  function handleSelect(selectedButton) {
+    setSekectedTopic(selectedButton);
+    //console.log(selectedTopic);
   }
   return (
     <div>
@@ -35,13 +39,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
           </menu>
         </section>
-        Dynamic Content
+        {selectedTopic}
       </main>
     </div>
   );
