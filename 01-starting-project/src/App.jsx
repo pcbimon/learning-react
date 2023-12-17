7b1150660801cb34e730ad7a4ab69f9f014e9f38
pcbimon <patipat.che@mahidol.edu>
2023-12-17 15:01:52 +0700

    disaply tab content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,12 +3,25 @@ import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 function App() {
   //state
-  const [ selectedTopic,setSekectedTopic ] = useState('Please click a button');
+  const [ selectedTopic,setSekectedTopic ] = useState();
   function handleSelect(selectedButton) {
     setSekectedTopic(selectedButton);
     //console.log(selectedTopic);
+  }
+  let tabContent = <p>Please select a topic.</p>;
+  if(selectedTopic){
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -44,8 +57,8 @@ function App() {
             <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
-        {selectedTopic}
       </main>
     </div>
   );
