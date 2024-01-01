36bb5c133cc839084fed47a91e8dfb86a881be1c
pcbimon <patipat.che@mahidol.edu>
2024-01-01 22:22:37 +0700

    gameboard onclick board

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 const initalGameBoard = [
     [null, null, null], 
     [null, null, null], 
     [null, null, null]
 ];
 export default function GameBoard(){
+    const [gameBoard,setGameBoad] = useState(initalGameBoard);
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoad((prevGameBoard)=>{
+            const updatedGameBoard = [...prevGameBoard.map(innerArr=>[...innerArr])];
+            updatedGameBoard[rowIndex][colIndex] = 'X';
+            return updatedGameBoard;
+        });
+    }
     return (
         <ol id="game-board">
-            {initalGameBoard.map((row, rowIndex) => 
+            {gameBoard.map((row, rowIndex) => 
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => 
-                        <li key={colIndex}><button>{playerSymbol}</button></li>
+                        <li key={colIndex}>
+                            <button onClick={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                        </li>
                     )}
                 </ol>
             </li>
