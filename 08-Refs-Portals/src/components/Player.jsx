74d280cc14db45796c297d95c0359996da2f5f11
pcbimon <patipat.che@mahidol.edu>
2024-01-03 16:46:43 +0700

    reset when submit name

--- 08-Refs-Portals/src/components/Player.jsx
+++ 08-Refs-Portals/src/components/Player.jsx
@@ -5,12 +5,13 @@ export default function Player() {
   const [enteredPlayerName,setEnteredPlayerName] = useState(null);
   function handleClick(){
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
   return (
     <section id="player">
       <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input ref={playerName} type="text" onChange={handleNameChange} value={enteredPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
